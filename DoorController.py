4f851f177ec04df9865856f7da3284f759e4a1de
Nicolas Lizotte <nlizotte@npxconnect.com>
2017-07-21 11:36:50 -0400

    release the motors after open and close

--- DoorController.py
+++ DoorController.py
@@ -12,13 +12,22 @@ myStepper.setSpeed(30)
 
 @application.route("/open")
 def open_door():
-    myStepper.step(250, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
+    myStepper.step(275, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
+    turnOffMotors()
     return "Door is open!"
 
 @application.route("/close")
 def close_door():
-    myStepper.step(250, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
+    myStepper.step(275, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
+    turnOffMotors()
     return "Door is closed!"
+
+
+def turnOffMotors():
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
